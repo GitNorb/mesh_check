becb9059bd0039b673eba1542e9022cb7f28951c
GitNorb <nhservice@mailbox.org>
2016-11-06 20:59:22 +0100

    Update mesh_check.sh

--- mesh_check.sh
+++ mesh_check.sh
@@ -1,37 +1,31 @@
 #!/bin/ash
 
+# Bitte beachten: Das Skript ist gerade in der Testphase.
+
+
 # next Node
 IP="2a01:198:70a:ff::FFFE"
 
-#fastd1
+# fastd1
 #IP="2a01:198:70a:ff::100:1"
 
 DATE=$(date +%d.%m.%Y,%H:%M:%S)
 LOGOUTPUT="/tmp/mesh_check.log"
 
-# Problem mit for-Schleife in ash
-#for (( i=0 ; i>3 ; i++ ))
-#do
-        echo "try $i"
-        echo "start ping to $IP"
-        ping -c 10 "$IP" > /dev/null # 10x Ping
-        if [ $? -eq 0 ]; then # Erfolgreich?
-                # Ja, Ende
-                echo "$IP online"
-                echo "exit"
-                #echo "$DATE exit">>"$LOGOUTPUT"               
-                exit 0
-        else          
-                # Nein.                   
-                echo "$IP offline"
-                #if [ $i -lt 2 ]; then
-                #        echo "sleep 1m"
-                #        sleep 1m
-                #fi
-        fi                        
-#done
-
-# Router hat Mesh verloren                
-echo "$DATE restart network">>"$LOGOUTPUT"
-echo "restart network"                    
-/etc/init.d/network restart
+echo "start ping to $IP"
+ping -c 10 "$IP" > /dev/null # 10x Ping
+if [ $? -eq 0 ]; then # Erfolgreich?
+    # Ja, Ende
+    echo "$IP online"
+    echo "exit"
+    #echo "$DATE exit">>"$LOGOUTPUT"               
+    exit 0
+else          
+    # Nein.                   
+    # Router hat Mesh verloren                
+    echo "$IP offline"
+    echo "$DATE restart network">>"$LOGOUTPUT"
+    echo "restart network"                    
+    /etc/init.d/network restart
+fi
+exit 0
